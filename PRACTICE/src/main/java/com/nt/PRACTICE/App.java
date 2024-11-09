0fa25a20afca8b55b840eebdd8ab3f7fc2e2b6fd
pavank-123 <pavan.kancharla0054@gmail.com>
2024-11-09 11:59:36 +0530

    dev1 file given

--- PRACTICE/src/main/java/com/nt/PRACTICE/App.java
+++ PRACTICE/src/main/java/com/nt/PRACTICE/App.java
@@ -6,8 +6,15 @@ package com.nt.PRACTICE;
  */
 public class App 
 {
+	public int sum(int a,int b) {
+		return a+b;
+	}
+	
+	
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( "Hello git" );
+        
     }
 }
